629d8be845440b54ca80d137e4aeb5cd3d9efc76
Your Name <farjanalubna.al@gmailo.com>
2019-07-16 21:46:34 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// init Isotope
+var $grid = $('.grid').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+/* count to plugin */
+        $('.timer').countTo();
